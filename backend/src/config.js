028527f8bf3c78811b0ecd2936411fa1c7a05aed
Duce2 <99472497+Duce2@users.noreply.github.com>
2022-04-12 14:52:38 -0700

    backend

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const basePath = process.cwd();
 const fs = require("fs");
 const { MODE } = require(`${basePath}/constants/blend_mode.js`);
@@ -6,67 +7,76 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "Drift Queens";
+const namePrefix = "YOUR COLLECTION NAME";
 const description = "Remember to replace this description";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 7500,
+    growEditionSizeTo: 5,
     layersOrder: [
       { name: "Background" },
-      { name: "Car Colors" },
-      { name: "Body" },
-      { name: "Tattoo" },
-      { name: "Outfits" },
-      { name: "Hair" },
-      { name: "Eyes" },
-      { name: "sunglasses" },
-      { name: "Lolipop" },
-      { name: "accessories" },
-      
+      { name: "Eyeball" },
+      { name: "Eye color" },
+      { name: "Iris" },
+      { name: "Shine" },
+      { name: "Bottom lid" },
+      { name: "Top lid" },
     ],
   },
 ];
 
-const shuffleLayerConfigurations = false;
+const shuffleLayerConfigurations = true;
 
 const debugLogs = false;
 
 const format = {
-  width: 2300,
-  height: 2000,
+  width: 512,
+  height: 512,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://www.driftqueensmint.com/", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
+
 // ** REQUIRED **
-const AUTH = "36e234b3-b2db-46a4-ba88-c2051833fcc5";
+const AUTH = process.env.NFTPORT_API_KEY; // Set this in the .env file to prevent exposing your API key when pushing to Github
 const LIMIT = 2; // Your API key rate limit
-const CONTRACT_NAME = 'DriftQueens';
-const CONTRACT_SYMBOL = 'DQ';
-const CONTRACT_TYPE = 'erc721';
-const MINT_TO_ADDRESS = '0x4235495C0ed2681c8e2F0EfBd90DFEBCFc3Dbb73';
-const CHAIN = 'rinkeby';
-const METADATA_UPDATABLE = false; // set to false if you don't want to allow metadata updates after minting
+const CHAIN = 'rinkeby'; // only rinkeby or polygon
+
+// REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
+const CONTRACT_NAME = 'CRYPTOPUNKS';
+const CONTRACT_SYMBOL = 'CP';
+const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
+const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
+const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
+const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
+const MINT_PRICE = 1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+
+// REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
+const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+
+// OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
+const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0x4235495C0ed2681c8e2F0EfBd90DFEBCFc3Dbb73"; // Address that will receive the royalty
+const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+const BASE_URI = null; // only update if you want to manually set the base uri
+const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
+const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
-const GENERIC_TITLE = "Unknown"; // Replace with what you want the generic titles to say.
-const GENERIC_DESCRIPTION = "Unknown"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = [
-  "https://cdn.discordapp.com/attachments/788152036931403776/961143758295023627/dq_wheel.png",
-]; // Replace with your generic image(s). If multiple, separate with a comma.
-const REVEAL_PROMPT = false; // Set to false if you want to disable the prompt to confirm each reveal.
-const INTERVAL = 900000; // Milliseconds. This is the interval for it to check for sales and reveal the NFT. 900000 = 15 minutes.
+const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
+const GENERIC_DESCRIPTION = "REPLACE THIS"; // Replace with what you want the generic descriptions to say.
+const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
@@ -167,18 +177,24 @@ module.exports = {
   AUTH,
   LIMIT,
   CONTRACT_ADDRESS,
-  MINT_TO_ADDRESS,
+  OWNER_ADDRESS,
+  TREASURY_ADDRESS,
   CHAIN,
   GENERIC,
   GENERIC_TITLE,
   GENERIC_DESCRIPTION,
   GENERIC_IMAGE,
-  INTERVAL,
   CONTRACT_NAME,
   CONTRACT_SYMBOL,
-  CONTRACT_TYPE,
-  REVEAL_PROMPT,
   METADATA_UPDATABLE,
   ROYALTY_SHARE,
   ROYALTY_ADDRESS,
+  MAX_SUPPLY,
+  MINT_PRICE,
+  TOKENS_PER_MINT,
+  PRESALE_MINT_START_DATE,
+  PUBLIC_MINT_START_DATE,
+  BASE_URI,
+  PREREVEAL_TOKEN_URI,
+  PRESALE_WHITELISTED_ADDRESSES
 };
